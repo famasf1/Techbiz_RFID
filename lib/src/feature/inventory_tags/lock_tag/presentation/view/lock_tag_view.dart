dda7b92557bd6f06cade9cd0f2463deae3d1e639
Worapod's Mac <info@techbusiness.co.th>
2025-03-04 17:57:26 +0700

    [minor] => add lock tag view

---
+++ lib/src/feature/inventory_tags/lock_tag/presentation/view/lock_tag_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class LockTagView extends ConsumerStatefulWidget {
+  const LockTagView({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _LockTagViewState();
+}
+
+class _LockTagViewState extends ConsumerState<LockTagView> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
